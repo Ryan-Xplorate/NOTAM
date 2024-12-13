3e0de76a4115afc5fb92c304d081407325a2c8fb
Ryan-Xplorate <ryan.midgley@xplorate.io>
2024-12-13 12:21:31 +1000

    Update footer.py

--- footer.py
+++ footer.py
@@ -22,8 +22,8 @@ def footer():
             text-align: center;
             margin: 20px;
             padding: 0 20px;
-            margin-left: 25%;  /* Offset text to avoid sidebar */
-            max-width: 75%;  /* Ensure text doesn't stretch too far */
+            margin-left: 40%;  /* Offset text to avoid sidebar */
+            max-width: 60%;  /* Ensure text doesn't stretch too far */
         }
         .main .block-container {
             padding-bottom: 5rem;
